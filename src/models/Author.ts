c7ca0e60c2224bfb4f1a22b14b4bfbfb1a1e53bc
Ing-China <peschina6677@gmail.com>
2024-10-06 15:38:48 +0700

    [API] Authors, Books, BookTypes, Favorites, Recommended, Trending,

---
+++ src/models/Author.ts
@@ -0,0 +1,30 @@
+import mongoose, { Document, Schema } from "mongoose";
+
+export interface IAuthor extends Document {
+  name: string;
+  age: string;
+  bookCount: string;
+  bio: string;
+  isActive?: boolean;
+  image: string;
+  bookType: mongoose.Types.ObjectId;
+}
+
+const AuthorSchema: Schema<IAuthor> = new Schema(
+  {
+    name: { type: String, required: true },
+    age: { type: String, required: true },
+    bookCount: { type: String, required: true },
+    bio: { type: String, required: true },
+    isActive: { type: Boolean, default: true },
+    image: { type: String, required: true },
+    bookType: { type: Schema.Types.ObjectId, ref: "BookType" },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Author =
+  mongoose.models.Author || mongoose.model<IAuthor>("Author", AuthorSchema);
+export default Author;
